cf94f82b8b2e520f0de9c99adfe1cb33f39df648
IsaelFatamaDev <isael.fatama@vallegrande.edu.pe>
2023-11-25 15:20:00 -0500

    v1.5

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 //TODO: Conexiones de las tablas : Mysql
 
-const express = require('express');
-const http = require('http');
-const path = require('path');
-const mysql = require('mysql');
-const cors = require('cors');
+import express from 'express';
+import http from 'http';
+import path from 'path';
+import mysql from 'mysql';
+import cors from 'cors';
 
 const app = express();
 const web = http.createServer(app);
 const port = 3000;
+
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import { methods as authenticacion } from "./controladores/authenticacion.js";
+import { methods as autoriza } from "./middlewares/autoriza.js";
 
 const conexion = mysql.createConnection({
       host: "localhost",
@@ -17,29 +22,25 @@ const conexion = mysql.createConnection({
       password: "admin",
 });
 
-conexion.connect(function (err) {
+conexion.connect((err) => {
       if (err) {
-            throw error;
+            throw err;
       } else {
-            console.log("Conexion exitosa")
+            console.log("Conexion exitosa");
       }
 });
 
 //* Configuracion de las rutas
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'assets')));
 
 //* Configuracion de las carpetas
-
 app.use('/assets', express.static('assets/'));
-
 
 const staticPath = path.join(__dirname, 'assets');
 const pagePath = (section, page) => path.join(__dirname, `../assets/pages/${section}/${page}`);
-
 
 // Configuración de las rutas y carpetas estáticas
 app.use(express.urlencoded({ extended: true }));
@@ -51,6 +52,14 @@ app.use(express.static(staticPath));
 app.get('/', (req, res) => {
       res.sendFile(path.join(__dirname, '../index.html'));
 });
+
+//RUTAS
+app.get("/login", autoriza.soloPublico, (req, res) => res.sendFile(__dirname + "/paginas/login.html"));
+app.get("/register", autoriza.soloPublico, (req, res) => res.sendFile(__dirname + "/paginas/register.html"));
+app.get("/admin", autoriza.soloAdmin, (req, res) => res.sendFile(__dirname + "/paginas/admin/admin.html"));
+app.post("/api/login", authenticacion.login);
+app.post("/api/register", authenticacion.register);
+
 
 app.get('/:page', (req, res) => {
       const page = req.params.page;
@@ -72,25 +81,24 @@ web.listen(port, () => {
       console.log("Servidor escuchando en el puerto: http://localhost:" + port);
 });
 
-
 //* * * * * * * * * * * CONEXIÓN BASE DE DATOS : FECHAS CÍVICAS * * * * * * * * * * * //
 
 app.get("/api/dates/:current", (req, res) => {
       var request = req.params.current;
-      conexion.query("SELECT NOMBRE, LUGAR, date_format(FECHA, '%d/%m/%Y') AS FECHA, DESCRIPCION FROM FECHAS_CIVICAS WHERE FECHA = ?", [request], function (err, row, fields) {
+      conexion.query("SELECT NOMBRE, LUGAR, date_format(FECHA, '%d/%m/%Y') AS FECHA, DESCRIPCION FROM FECHAS_CIVICAS WHERE FECHA = ?", [request], (err, row, fields) => {
             if (err) {
                   throw err;
-            } else if (row[0] != null) {
+            } else if (row[0] !== null) {
                   console.log("Respuesta JSON:", row[0]);
                   res.json(row[0]);
             } else {
                   console.log("Respuesta JSON nula");
                   res.json(null);
             }
-      })
+      });
 });
 
-app.post("/validar", function (req, res) {
+app.post("/validar", (req, res) => {
       const datos = req.body;
       let nombres = datos.name;
       let email = datos.email;
@@ -101,7 +109,7 @@ app.post("/validar", function (req, res) {
       let registrar = `INSERT INTO CONTACTO (nombres, email, asunto, mensaje, pendiente) 
                       VALUES ('${nombres}', '${email}', '${asunto}', '${mensaje}', 'P')`;
 
-      conexion.query(registrar, function (error) {
+      conexion.query(registrar, (error) => {
             if (error) {
                   throw error;
             } else {
@@ -112,6 +120,7 @@ app.post("/validar", function (req, res) {
 
       console.log(datos);
 
-      let consulta = 'SELECT * FROM CONTACTO'
+      let consulta = 'SELECT * FROM CONTACTO';
       console.log(consulta);
 });
+
